aa936ef575f448844b27980071bc9779ed8e147b
paganaye <paganaye@78469ee7-fb8b-4669-8d69-407dd4d36f23>
2012-10-31 23:24:34 +0000

    Tests for Decimals
    git-svn-id: https://subversion.assembla.com/svn/eval4/trunk@8 78469ee7-fb8b-4669-8d69-407dd4d36f23

---
+++ Eval4Tests/CommonTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Eval4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eval4.CSharpTests
+{
+    [TestClass]
+    public class CommonTests
+    {
+        static VbEvaluator evVB;
+        static CSharpEvaluator evCSharp;
+
+        static CommonTests()
+        {
+            evVB = new VbEvaluator();
+            evCSharp = new CSharpEvaluator();
+            InitEvaluator(evVB);
+            InitEvaluator(evCSharp);
+
+        }
+
+        private static void InitEvaluator(Eval4.Core.Evaluator res)
+        {
+        }
+
+        public void TestFormula<T>(Eval4.Core.Evaluator ev, string formula, T expectedResult)
+        {
+            var actualResult = ev.Eval(formula);
+            Assert.AreEqual(expectedResult, actualResult, formula);
+        }
+
+        public void TestFormula<T>(string formula, T expectedResult)
+        {
+            TestFormula(evCSharp, formula, expectedResult);
+            formula = formula.Replace("[", "(").Replace("]", ")");
+            TestFormula(evVB, formula, expectedResult);
+        }
+
+
+        [TestMethod]
+        public void TestDecimal()
+        {
+            TestFormula("1.5", 1.5);
+            TestFormula("0.5", 0.5);
+            TestFormula(".5", .5);
+            TestFormula("-.5", -.5);
+            TestFormula("-0.5", -0.5);
+        }
+
+    }
+}
