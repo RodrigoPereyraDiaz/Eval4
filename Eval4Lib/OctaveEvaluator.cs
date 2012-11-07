477c61070b95e125b6570cfa1df6f515dd174f2e
paganaye <paganaye@78469ee7-fb8b-4669-8d69-407dd4d36f23>
2012-11-07 17:06:00 +0000

    git-svn-id: https://subversion.assembla.com/svn/eval4/trunk@37 78469ee7-fb8b-4669-8d69-407dd4d36f23

---
+++ Eval4Lib/OctaveEvaluator.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Eval4.Core;
+
+namespace Eval4
+{
+    public enum OctaveToken
+    {
+        None
+    }
+
+    public class OctaveEvaluator : Core.Evaluator<OctaveToken>
+    {
+        protected internal override EvaluatorOptions Options
+        {
+            get
+            {
+                return EvaluatorOptions.BooleanLogic
+                    | EvaluatorOptions.CaseSensitive
+                    //| EvaluatorOptions.DateTimeValues
+                    | EvaluatorOptions.DoubleValues
+                    | EvaluatorOptions.IntegerValues
+                    | EvaluatorOptions.ObjectValues
+                    | EvaluatorOptions.StringValues;
+            }
+        }
+
+        public override bool UseParenthesisForArrays
+        {
+            get { return false; }
+        }
+
+        public override Token ParseToken()
+        {
+            switch (mCurChar)
+            {
+                case '%':
+                    NextChar();
+                    return NewToken(TokenType.OperatorModulo);
+
+                case '&':
+                    NextChar();
+                    if (mCurChar == '&')
+                    {
+                        NextChar();
+                        return NewToken(TokenType.OperatorAndAlso);
+                    }
+                    return NewToken(TokenType.OperatorAnd);
+
+                case '?':
+                    NextChar();
+                    return NewToken(TokenType.OperatorIf);
+
+                case '=':
+                    NextChar();
+                    if (mCurChar == '=')
+                    {
+                        NextChar();
+                        return NewToken(TokenType.OperatorEQ);
+                    }
+                    return NewToken(TokenType.OperatorAssign);
+
+                case '!':
+                    NextChar();
+                    if (mCurChar == '=')
+                    {
+                        NextChar();
+                        return NewToken(TokenType.OperatorNE);
+                    }
+                    return NewToken(TokenType.OperatorNot);
+
+                case '^':
+                    NextChar();
+                    return NewToken(TokenType.OperatorXor);
+
+                case '|':
+                    NextChar();
+                    if (mCurChar == '|')
+                    {
+                        NextChar();
+                        return NewToken(TokenType.OperatorOrElse);
+                    }
+                    return NewToken(TokenType.OperatorOr);
+                case ':':
+                    NextChar();
+                    return NewToken(TokenType.OperatorColon);
+                default:
+                    return base.ParseToken();
+
+            }
+        }
+
+        public override Token CheckKeyword(string keyword)
+        {
+            {
+                switch (keyword.ToString())
+                {
+                    case "true":
+                        return NewToken(TokenType.ValueTrue);
+
+                    case "false":
+                        return NewToken(TokenType.ValueFalse);
+
+                    default:
+                        return base.CheckKeyword(keyword);
+                }
+            }
+        }
+
+        internal override void ParseRight(Token tk, int opPrecedence, IHasValue Acc, ref IHasValue valueLeft)
+        {
+            var tt = tk.Type;
+            switch (tt)
+            {
+                case TokenType.OperatorIf:
+                    NextToken();
+                    IHasValue thenExpr = ParseExpr(null, 0);
+                    Expect(TokenType.OperatorColon, "Missing : in ? expression test ? valueIfTrue : valueIfFalse.");
+                    IHasValue elseExpr = ParseExpr(null, 0);
+                    var t = typeof(OperatorIfExpr<>).MakeGenericType(thenExpr.SystemType);
+
+                    valueLeft = (IHasValue)Activator.CreateInstance(t, valueLeft, thenExpr, elseExpr);
+                    break;
+                default:
+                    base.ParseRight(tk, opPrecedence, Acc, ref valueLeft);
+                    break;
+            }
+        }
+
+        public override int GetPrecedence(Token<OctaveToken> token, bool unary)
+        {
+            var tt = token.Type;
+            //http://msdn.microsoft.com/en-us/library/aa691323(v=vs.71).aspx
+            switch (tt)
+            {
+                case TokenType.Dot:
+                case TokenType.OpenParenthesis:
+                case TokenType.OpenBracket:
+                case TokenType.New:
+
+                    // 	Primary	
+                    //x.y  f(x)  a[x]  x++  x--  new
+                    //typeof  checked  unchecked
+                    return 15;
+
+                case TokenType.OperatorPlus:
+                case TokenType.OperatorMinus:
+                    return (unary ? 14 : 12);
+
+                case TokenType.OperatorNot:
+                case TokenType.OperatorTilde:
+                    // 	Unary	
+                    //+  -  !  ~  ++x  --x  (T)x
+                    return 14;
+
+                case TokenType.OperatorMultiply:
+                case TokenType.OperatorDivide:
+                case TokenType.OperatorModulo:
+                    // 	Multiplicative	
+                    //*  /  %
+                    return 13;
+
+                //case TokenType.Operator_plus:
+                //case TokenType.Operator_minus:
+                // 	Additive	
+                //  +  -
+                //  return 12;
+
+                case TokenType.ShiftLeft:
+                case TokenType.ShiftRight:
+                    // 	Shift	
+                    //<<  >>
+                    return 11;
+
+                case TokenType.OperatorLT:
+                case TokenType.OperatorLE:
+                case TokenType.OperatorGE:
+                case TokenType.OperatorGT:
+                    // 	Relational and type testing	
+                    //<  >  <=  >=  is  as
+                    return 10;
+
+                case TokenType.OperatorEQ:
+                case TokenType.OperatorNE:
+                    // 	Equality	
+                    //==  !=
+                    return 9;
+
+                case TokenType.OperatorAnd:
+                    // 	Logical AND	
+                    //&
+                    return 8;
+
+                case TokenType.OperatorXor:
+                    // 	Logical XOR	
+                    //^
+                    return 7;
+
+                case TokenType.OperatorOr:
+                    // 	Logical OR	
+                    //|
+                    return 6;
+
+                case TokenType.OperatorAndAlso:
+                    // 	Conditional AND	
+                    //&&
+                    return 5;
+                case TokenType.OperatorOrElse:
+                    // 	Conditional OR	
+                    //||
+                    return 4;
+                case TokenType.OperatorIf:
+                    // 	Conditional	
+                    //?:
+                    return 3;
+                case TokenType.OperatorAssign:
+                    // 	Assignment	
+                    //=  *=  /=  %=  +=  -=  <<=  >>=  &=  ^=  |=
+                    return 2;
+                case TokenType.OperatorColon:
+                case TokenType.CloseParenthesis:
+                case TokenType.CloseBracket:
+                case TokenType.Comma:
+                    return 0;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
