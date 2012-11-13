798b5e32d0c14ba3966332274435a2146319503b
paganaye <paganaye@78469ee7-fb8b-4669-8d69-407dd4d36f23>
2012-11-13 20:38:10 +0000

    git-svn-id: https://subversion.assembla.com/svn/eval4/trunk@49 78469ee7-fb8b-4669-8d69-407dd4d36f23

---
+++ Eval5Demo/Evaluator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eval5Demo
+{
+    class Evaluator
+    {
+        internal Func<T> Parse<T>(string source)
+        {
+            T result = default(T);
+            return (() => result);
+        }
+
+        public T Eval<T>(string source)
+        {
+            var result= Parse<T>(source);
+            return result();
+        }
+
+    }
+}
---
+++ Eval5Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eval5Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I would like to make the evaluators definition as simple as possible
+            // to allow extensibility
+            // to avoid BNF rules syntax as there are already many tools to do that
+
+            var ev = new MyEvaluator();
+            var val = ev.Eval<int>("1+2*3");
+
+            
+        }
+
+        public class MyEvaluator : Evaluator
+        {
+            public MyEvaluator()
+            {
+            }
+
+        }
+    }
+}
